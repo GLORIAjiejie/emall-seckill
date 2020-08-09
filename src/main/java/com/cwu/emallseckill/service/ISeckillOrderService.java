1462a7cbc318417e4a8b688282a4c234baedbd80
GLORIAjiejie <2108295535@qq.com>
2020-08-09 23:17:37 +0800

    Update README.md

--- src/main/java/com/cwu/emallseckill/service/ISeckillOrderService.java
+++ src/main/java/com/cwu/emallseckill/service/ISeckillOrderService.java
@@ -5,6 +5,8 @@ import com.cwu.emallseckill.entity.OrderInfo;
 import com.cwu.emallseckill.entity.SeckillOrder;
 import com.cwu.emallseckill.entity.User;
 
+import java.util.List;
+
 public interface ISeckillOrderService {
     /** 隐藏了秒杀路径，验证路径 */
     boolean checkPath(User user, long goods,String path);
@@ -18,7 +20,12 @@ public interface ISeckillOrderService {
     /** 根据用户和用户选择的秒杀商品id获取秒杀链接中的路径 */
     String createSeckillPath(User user, long goodsId);
 
-    long getSeckillResult(int id, long goodsId);
+    /** 根据用户id和商品id查看是否成功的结果*/
+    long getSeckillResult(long userId, long goodsId);
 
+    /** 查询某个用户下的订单列表 */
+    List<OrderInfo> getOrderList(User user);
+
+    /** 根据订单id查询订单信息 */
     OrderInfo getOrderInfo(long orderId);
 }
